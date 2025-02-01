ff1cf373fdf2adff8b252dca5345512b9893cc36
afzalfaizi <afzaalm993@gmail.com>
2025-02-01 21:53:56 +0500

    Update pricing structure in PriceCard component to reflect hourly rates

--- app/price/page.tsx
+++ app/price/page.tsx
@@ -12,9 +12,9 @@ const Price = () => {
                 </div>
                 <div className=' w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 items-center
              pt-[4rem] md:pt-[7rem]'>
-                    <PriceCard plane="Basic" price="10$" save="25%" bg="bg-red-900" />
-                    <PriceCard plane="Popular" price="20$" save="35%" bg="bg-green-900" />
-                    <PriceCard plane="Premium" price="30$" save="45%" bg="bg-blue-900" />
+                    <PriceCard plane="Basic" price="25$/hour" save="25%" bg="bg-red-900" />
+                    <PriceCard plane="Popular" price="30$/hour" save="35%" bg="bg-green-900" />
+                    <PriceCard plane="Premium" price="50$/hour" save="45%" bg="bg-blue-900" />
                 </div>
 
             </div>
